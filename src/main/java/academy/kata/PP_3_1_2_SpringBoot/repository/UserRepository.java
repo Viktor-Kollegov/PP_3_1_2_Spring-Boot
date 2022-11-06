0a06e861977eca7077e5f4aa1ea47f5a6ab38c66
Viktor Kollegov <rocket22@yandex.ru>
2022-11-06 14:13:04 +0300

    JpaRepository implementation, bootstrap upgraded to 5.2.2

--- src/main/java/academy/kata/PP_3_1_2_SpringBoot/repository/UserRepository.java
+++ src/main/java/academy/kata/PP_3_1_2_SpringBoot/repository/UserRepository.java
@@ -2,7 +2,9 @@ package academy.kata.PP_3_1_2_SpringBoot.repository;
 
 import academy.kata.PP_3_1_2_SpringBoot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
